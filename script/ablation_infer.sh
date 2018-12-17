bb1abc7255246178f4b0527fea312ec7eeb3bf80
Namhyuk Ahn <nmhkahn@gmail.com>
2018-12-17 17:22:05 +0900

    Add script

--- script/ablation_infer.sh
+++ script/ablation_infer.sh
@@ -1,7 +1,10 @@
 for d in checkpoint/* ; do
+    IFS='/' read -ra PATHS <<< "$d"
+    IFS='_' read -ra name <<< "${PATHS[-1]}"
+
     CUDA_VISIBLE_DEVICES=$1 python carnpp/inference.py \
-        --model ablation. \
+        --model ablation.$name \
         --ckpt $d/400000.pth \
         --data ./dataset/Set14 \
-        --sample_dir ./sample/$d
+        --sample_dir ./sample/${PATHS[-1]}
 done
